2b003ae912bd4d7d0df8a11ca1779daaf0e34762
diagmatrix <manuelgbal1@gmail.com>
2024-10-25 20:30:30 +0200

    Start card managing

---
+++ card.go
@@ -0,0 +1,168 @@
+package main
+
+import (
+	"errors"
+	"time"
+)
+
+type Card struct {
+	layout          string
+	SubCards        []Card
+	Name            string
+	CMC             float64
+	Colors          []string
+	ColorIdentity   []string
+	Types           []string
+	Subtypes        []string
+	SetID           string
+	OracleText      string
+	keywords        []string
+	CollectorNumber string
+	ArtistID        int
+}
+
+type RawCard struct {
+	// Core card fields
+	ArenaID           int    `json:"arena_id"`
+	ID                string `json:"id"`
+	MtgoID            int    `json:"mtgo_id"`
+	MtgoFoilID        int    `json:"mtgo_foil_id"`
+	MultiverseIDs     []int  `json:"multiverse_id"`
+	TgplayerID        int    `json:"tcgplayer_id"`
+	TgcplayerEtchedID int    `json:"tcgplayer_etched_id"`
+	CardmarketID      int    `json:"cardmarket_id"`
+	Object            string `json:"object"`
+	Layout            string `json:"layout"`
+	OracleID          string `json:"oracle_id"`
+	PrintsSearchURI   string `json:"prints_search_uri"`
+	RulingsURI        string `json:"rulings_uri"`
+	ScryfallURI       string `json:"scryfall_uri"`
+	URI               string `json:"uri"`
+	// Gameplay fields
+	AllParts       []RelatedCard     `json:"all_parts"`
+	CardFaces      []CardFace        `json:"card_faces"`
+	CMC            float64           `json:"cmc"`
+	ColorIdentity  []string          `json:"color_identity"`
+	ColorIndicator []string          `json:"color_indicator"`
+	Colors         []string          `json:"colors"`
+	EdhrecRank     int               `json:"edhrec_rank"`
+	HandModifier   string            `json:"hand_modifier"`
+	Keywords       []string          `json:"keywords"`
+	Legalities     map[string]string `json:"legalities"`
+	LifeModifier   string            `json:"life_modifier"`
+	Loyalty        string            `json:"loyalty"`
+	ManaCost       string            `json:"mana_cost"`
+	Name           string            `json:"name"`
+	OracleText     string            `json:"oracle_text"`
+	PennyRank      int               `json:"penny_rank"`
+	Power          string            `json:"power"`
+	ProducedMana   []string          `json:"produced_mana"`
+	Reserved       bool              `json:"reserved"`
+	Toughness      string            `json:"toughness"`
+	TypeLine       string            `json:"type_line"`
+	// Print fields
+	Artist           string             `json:"artist"`
+	ArtistIDs        []int              `json:"artist_ids"`
+	AttractionLights []any              `json:"attribution_lights"` // TODO: What is this?
+	Booster          bool               `json:"booster"`
+	BorderColor      string             `json:"border_color"`
+	CardBackID       string             `json:"card_back_id"`
+	CollectorNumber  string             `json:"collector_number"`
+	ContentWarning   bool               `json:"content_warning"`
+	Digital          bool               `json:"digital"`
+	Finishes         []string           `json:"finishes"`
+	FlavorName       string             `json:"flavor_name"`
+	FlavorText       string             `json:"flavor_text"`
+	FrameEffects     []string           `json:"frame_effects"`
+	Frame            string             `json:"frame"`
+	FullArt          bool               `json:"full_art"`
+	Games            []string           `json:"games"`
+	HighresImage     bool               `json:"highres_image"`
+	IllustrationID   string             `json:"illustration_id"`
+	ImageStatus      string             `json:"image_status"`
+	ImageURIs        map[string]string  `json:"image_uris"`
+	Oversized        bool               `json:"oversized"`
+	Prices           map[string]float64 `json:"prices"`
+	PrintedName      string             `json:"printed_name"`
+	PrintedText      string             `json:"printed_text"`
+	PrintedTypeLine  string             `json:"printed_type_line"`
+	Promo            bool               `json:"promo"`
+	PromoTypes       []string           `json:"promo_types"`
+	PurchaseURIs     map[string]string  `json:"purchase_uris"`
+	Rarity           string             `json:"rarity"`
+	RelatedURIs      map[string]string  `json:"related_uris"`
+	ReleasedAt       time.Time          `json:"released_at"`
+	Reprint          bool               `json:"reprint"`
+	ScryfallSetURI   string             `json:"scryfall_set_uri"`
+	SetName          string             `json:"set_name"`
+	SetSearchURI     string             `json:"set_search_uri"`
+	SetType          string             `json:"set_type"`
+	SetURI           string             `json:"set_uri"`
+	Set              string             `json:"set"`
+	SetID            string             `json:"set_id"`
+	StorySpotlight   bool               `json:"story_spotlight"`
+	Textless         bool               `json:"textless"`
+	Variation        bool               `json:"variation"`
+	VariationOf      string             `json:"variation_of"`
+	Watermark        string             `json:"watermark"`
+}
+
+func (c RawCard) ToCard() (*Card, error) {
+	card := &Card{}
+	if c.Name == "" {
+		return nil, errors.New("Card name is required")
+	}
+	if c.SetID == "" {
+		return nil, errors.New("Set ID is required")
+	}
+	if c.CollectorNumber == "" {
+		return nil, errors.New("Collector number is required")
+	}
+	card.Name = c.Name
+	card.CMC = c.CMC
+	card.Colors = c.Colors
+	card.ColorIdentity = c.ColorIdentity
+	card.Types = []string{c.TypeLine} // TODO: Handle type - subtype
+	card.Subtypes = []string{}
+	card.SetID = c.SetID
+	card.OracleText = c.OracleText
+	card.keywords = c.Keywords
+	card.CollectorNumber = c.CollectorNumber
+	card.ArtistID = c.ArtistIDs[0] // TODO: WTF is this?
+	return card, nil
+}
+
+type CardFace struct {
+	Artist          string            `json:"artist"`
+	ArtistID        string            `json:"artist_id"`
+	CMC             float64           `json:"cmc"`
+	ColorIndicator  []string          `json:"color_indicator"`
+	Colors          []string          `json:"colors"`
+	Defense         string            `json:"defense"`
+	FlavorText      string            `json:"flavor_text"`
+	IllustrationID  string            `json:"illustration_id"`
+	ImageURIs       map[string]string `json:"image_uris"`
+	Layout          string            `json:"layout"`
+	Loyalty         string            `json:"loyalty"`
+	ManaCost        string            `json:"mana_cost"`
+	Name            string            `json:"name"`
+	Object          string            `json:"object"`
+	OracleID        string            `json:"oracle_id"`
+	OracleText      string            `json:"oracle_text"`
+	Power           string            `json:"power"`
+	PrintedName     string            `json:"printed_name"`
+	PrintedText     string            `json:"printed_text"`
+	PrintedTypeLine string            `json:"printed_type_line"`
+	Toughness       string            `json:"toughness"`
+	TypeLine        string            `json:"type_line"`
+	Watermark       string            `json:"watermark"`
+}
+
+type RelatedCard struct {
+	ID        string `json:"id"`
+	Object    string `json:"object"`
+	Component string `json:"component"`
+	Name      string `json:"name"`
+	TypeLine  string `json:"type_line"`
+	URI       string `json:"uri"`
+}
