0eba15f460bdc645a875df4932d33873d182885b
diagmatrix <manuelgbal1@gmail.com>
2024-11-12 23:53:51 +0100

    [MM] Refactor migrations table

--- init.sql
+++ init.sql
@@ -2,10 +2,14 @@
 
 -- Schema and tables for core functionality
 CREATE SCHEMA IF NOT EXISTS doorkeeper;
+-- Migrations table
 CREATE TABLE IF NOT EXISTS doorkeeper.migrations (
     id SERIAL PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    last_run TIMESTAMP,
+    executed BOOLEAN DEFAULT FALSE,
+    CONSTRAINT migrations_name_unique UNIQUE (name)
 );
 
 -- Schema for the cards
